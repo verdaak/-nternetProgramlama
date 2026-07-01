0e54bb11793c9ef7578e52da9c9c5857379cd2bf
verdaak <github_epostan@example.com>
2026-07-02 01:03:05 +0300

    Build işlemi tamamlandı, docs klasörü güncellendi

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pages için kritik
+  base: './', 
+  build: {
+    outDir: 'docs',
+  },
 })
